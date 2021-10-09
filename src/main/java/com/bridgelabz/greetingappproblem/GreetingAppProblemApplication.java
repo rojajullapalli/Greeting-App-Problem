1f1602e185bea6cac374fb198a71f57e0b3b0183
rojajullapalli <rojajules@gmail.com>
2021-10-09 13:12:04 +0530

    ADDED: GreetingContrroller api

---
+++ src/main/java/com/bridgelabz/greetingappproblem/GreetingAppProblemApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppProblemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/greetingappproblem/GreetingAppProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
