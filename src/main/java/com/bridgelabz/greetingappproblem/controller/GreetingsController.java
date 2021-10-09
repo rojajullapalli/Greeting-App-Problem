dd224ffe8728c1fec5f7eaa17f55bffcbad907f0
rojajullapalli <rojajules@gmail.com>
2021-10-09 13:22:30 +0530

    ADDED: Service Layer To Get Simple Message

--- src/main/java/com/bridgelabz/greetingappproblem/controller/GreetingsController.java
+++ src/main/java/com/bridgelabz/greetingappproblem/controller/GreetingsController.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.greetingappproblem.controller;
+import com.bridgelabz.greetingappproblem.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,5 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greetingsController")
 public class GreetingsController {
 
+    @Autowired
+    GreetingService greetingService;
+    @GetMapping("/greet")
+    public String getGreeting(){
+        return greetingService.getGreeting();
+    }
 
 }
